df814c260c9f0e1d0324eadd9f356fe06cacf8ce
krishnakalyan3 <skalyan@cloudera.com>
2020-05-21 17:56:53 +0200

    update code

---
+++ notebooks/01_EDA.R
@@ -0,0 +1,68 @@
+# install.packages(c("vroom", "tidyverse"))
+installed.packages("ggthemes")
+library(vroom)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+path = "/Users/kkalyan/github/airQo/data/"
+col_list_train <- list(location=col_factor(), target=col_double(), ID=col_factor())
+col_list_test <- list(location=col_factor(), ID=col_factor())
+
+raw_train <- vroom(paste0(path, "Train.csv"), delim = ",", col_types=col_list_train)
+raw_tests <- vroom(paste0(path, "Test.csv"), delim = ",", col_types=col_list_test)
+
+# precip
+precip <- unlist(str_split(raw_tests$precip[10], ","))
+plot(precip)
+
+# rel_humidity
+rel_humidity <- unlist(str_split(raw_tests$rel_humidity[10], ","))
+plot(rel_humidity)
+
+# wind_dir
+wind_dir <- unlist(str_split(raw_tests$wind_dir[10], ","))
+plot(wind_dir)
+
+# wind_spd
+wind_spd <- unlist(str_split(raw_tests$wind_spd[10], ","))
+plot(wind_spd)
+
+# atmos_press
+atmos_press <- unlist(str_split(raw_tests$atmos_press[10], ","))
+plot(atmos_press)
+
+# Average by location
+library(tibble)
+options(pillar.sigfig = 10)
+avg_by_location <- raw_train %>% group_by(location) %>% summarise(avg=mean(target))
+avg_by_location
+
+# Plot (Location, Target)
+p <- ggplot(raw_train, aes(location, target)) 
+p + geom_boxplot()
+
+# Plot (Location)
+p1 <- ggplot(raw_train) + geom_bar(aes(location))
+p1
+
+# Plot (Location)
+p2 <- ggplot(raw_train, aes(target, x= ""))
+p2 + geom_boxplot()
+
+
+# Submit mean for different locaitons
+submit <- raw_tests %>% select(c("ID", "location")) %>% 
+  mutate(target = case_when(location == "A"  ~ 57.65,
+                            location == "B"  ~ 49.86,
+                            location == "C"  ~ 97.77,
+                            location == "D"  ~ 58.15,
+                            location == "E"  ~ 37.80,
+                            TRUE ~ 0)) %>% 
+  select(c("ID", "target"))
+
+write_path = "/Users/kkalyan/github/airQo/submissions/"
+write.csv(submit, paste0(write_path,"r-baseline-mean-all.csv"), row.names = FALSE)
+# Remove NAs
+# Create Featuers
+# Plot
---
+++ notebooks/02_Boost.R
@@ -0,0 +1,61 @@
+#installed.packages("ggthemes")
+#install.packages('devtools')
+#install.packages("caret")
+
+library(vroom)
+library(tidyverse)
+library(catboost)
+library(caret)
+
+path <- "/Users/kkalyan/github/airQo/data/"
+raw_train <- vroom(paste0(path, "train-proc.csv"), delim = ",", col_types=list(location=col_factor()))
+raw_tests <- vroom(paste0(path, "test-proc.csv"), delim = ",", col_types=list(location=col_factor()))
+
+X <- raw_train %>% select(-c("target", "ID"))
+y <- raw_train$target
+
+params <- list(iterations = 7000, 
+               loss_function = 'RMSE', 
+               eval_metric='RMSE',
+               learning_rate=0.1,
+               od_wait=100,
+               use_best_model=TRUE)
+
+total_folds <- 20
+folds <- cut(seq(1,nrow(X)), breaks=total_folds, labels=FALSE)
+
+rmse_vec <- c()
+X_test <- raw_tests %>% select(-c( "ID", "target"))
+dtest <- catboost.load_pool(data=X_test)
+pred_mat <- matrix(0, nrow = 5035, ncol = total_folds)
+
+for(i in 1:total_folds){
+  #Segement your data by fold using the which() function 
+  testIndexes <- which(folds==1,arr.ind=TRUE)
+  X_train_fold <- X[-testIndexes, ]
+  y_train_fold <- y[-testIndexes]
+  
+  X_val_fold <- X[testIndexes, ]
+  y_val_fold <- y[testIndexes]
+  
+  dtrain <- catboost.load_pool(data=X_train_fold, label=y_train_fold)
+  dval <- catboost.load_pool(data=X_val_fold, label=y_val_fold)
+  
+  model <- catboost.train(dtrain, dval, params=params)
+  eval_preds <- catboost.predict(model, dval)
+  test_preds <- catboost.predict(model, dtest) 
+  
+  pred_mat[,i] <- test_preds
+  rmse_cat <- postResample(eval_preds, y_val_fold)
+  rmse_vec <- c(rmse_cat, rmse_vec)
+}
+
+# RMSE: 23.85
+mat_rmse <- matrix(rmse_vec, nrow=total_folds)
+rowMeans(mat_rmse)
+
+sub <- vroom(paste0(path, "sample_sub.csv"), delim = ",")
+sub$target <- rowMeans(pred_mat)
+
+write_path = "/Users/kkalyan/github/airQo/submissions/"
+write.csv(sub, paste0(write_path,"r-baseline-catboost-cv-7k-20cv.csv"), row.names = FALSE)
